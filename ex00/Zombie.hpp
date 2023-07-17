7b4459d730842eb7901d5a3b24d5daf50b14da11
aporlo <aporloporne@mail.com>
2023-07-17 15:00:55 +0700

    create file

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,20 @@
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+
+#include <iostream>
+#include <string>
+
+class Zombie {
+
+public:
+	Zombie();
+	Zombie(std::string name);
+
+
+
+private:
+
+	std::string		_name;
+};
+
+#endif
